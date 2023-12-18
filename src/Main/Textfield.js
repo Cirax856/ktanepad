896e6017aa05194157f9779d49681b9a3fd1eba5
Cirax856 <Michal.Schneider07@seznam.cz>
2023-12-18 06:38:41 +0100

    Line system bug fixes.

--- src/Main/Textfield.js
+++ src/Main/Textfield.js
@@ -10,13 +10,30 @@ export function Textfield({ textAreaRef })
     // line numbers state
     const [lines, setLines] = useState([0]);
 
-    function newInput(e)
+    function manageInput(e)
+    {
+        if (e.key === 'Enter') newLine(e);
+        if (e.key === 'ArrowUp') moveLine(e, 'up');
+        if (e.key === 'ArrowDown') moveLine(e, 'down');
+    }
+
+    function newLine(e)
     {
         let newId = parseInt(e.target.id.match(/\d+/)) + 1;
         setLines([...lines, newId]);
     }
 
     useEffect(() => document.getElementById(`Textfield__Input${lines[lines.length - 1]}`).focus(), [lines]);
+
+    function moveLine(e, dir)
+    {
+        let newId = parseInt(e.target.id.match(/\d+/));
+
+        if (dir === 'up' && newId !== 0) newId--;
+        if (dir === 'down' && newId !== lines.length - 1) newId++;
+
+        document.getElementById(`Textfield__Input${newId}`).focus();
+    }
 
     function lineClick(e)
     {
@@ -36,7 +53,7 @@ export function Textfield({ textAreaRef })
                 return (
                     <div className="Textfield__Line" id={index} onClick={event => { lineClick(event) }}>
                         <p className="Textfield__LineNumber" id={index}>{index + 1}</p>
-                        <input className="Textfield__Input" id={`Textfield__Input${index}`} onKeyDown={event => { if (event.key === 'Enter') newInput(event); }}></input>
+                        <input className="Textfield__Input" id={`Textfield__Input${index}`} onKeyDown={event => { manageInput(event); }}></input>
                     </div>
                 )
             })}
