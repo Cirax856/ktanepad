6d49a26bb3f3925685855312fd36f86a709caed0
Cirax856 <Michal.Schneider07@seznam.cz>
2023-12-25 11:29:08 +0100

    Hopfully finished the line system.

--- src/Main/Textfield.js
+++ src/Main/Textfield.js
@@ -1,5 +1,5 @@
 // import React
-import { useState, useEffect } from 'react';
+import { useState, useRef } from 'react';
 
 // importing CSS
 import './css/TextfieldCSS.css';
@@ -7,91 +7,48 @@ import './css/TextfieldCSS.css';
 // main function
 export function Textfield()
 {
-    const [lines, setLines] = useState([{ id: 1, content: '' }]);
-    const [focusedLine, setFocusedLine] = useState(1);
+    const [lineNumbers, setLineNumbers] = useState(['']);
 
-    function createLine(lineId)
+    const lineNumbersRef = useRef(null);
+    const textAreaRef = useRef(null);
+
+    function handleTextareaScroll()
     {
-        setLines((prevLines) =>
-        {
-            const index = prevLines.findIndex((line) => line.id === lineId);
-            const newLine = { id: prevLines.length + 1, content: '' };
-
-            const updatedLines = [
-                ...prevLines.slice(0, index + 1),
-                newLine,
-                ...prevLines.slice(index + 1),
-            ];
-
-            setFocusedLine(focusedLine + 1);
-
-            return updatedLines.map((line, idx) => ({ ...line, id: idx + 1 }));
-        });
-    };
-
-    useEffect(() =>
-    {
-        document.getElementById(`Textfield__Input${focusedLine}`).focus();
-    }, [focusedLine]);
-
-    function manageInput(e, lineId)
-    {
-        setLines((prevLines) =>
-            prevLines.map((line) =>
-                line.id === lineId ? { ...line, content: e.target.value } : line
-            )
-        );
-    };
-
-    function moveLine(e, dir)
-    {
-        if (dir === 'up' && focusedLine !== 1) setFocusedLine(focusedLine - 1);
-        if (dir === 'down' && focusedLine !== lines.length) setFocusedLine(focusedLine + 1);
+        lineNumbersRef.current.scrollTop = textAreaRef.current.scrollTop;
     }
 
-    function deleteLine(e, backspace)
+    function handleTextareaTextChange(e)
+    {
+        const text = e.target.value;
+        const lines = text.split('\n');
+        setLineNumbers(lines);
+    }
+
+    function addTab(e)
     {
         e.preventDefault();
 
-        if (focusedLine !== 1)
-        {
-            setLines((prevLines) =>
-            {
-                const index = prevLines.findIndex((line) => line.id === focusedLine);
+        const { selectionStart, selectionEnd } = textAreaRef.current;
+        const text = textAreaRef.current.value;
 
-                if (backspace) prevLines.filter((line) => line.id === focusedLine - 1)[0].content += e.target.value;
-                lines.splice(index, 1);
-                return lines.map((line, idx) => ({ ...line, id: idx + 1 }));
-            });
+        const updatedText = text.substring(0, selectionStart) + '\t' + text.substring(selectionEnd);
 
-            setFocusedLine(focusedLine - 1);
-        }
+        textAreaRef.current.value = updatedText;
+        textAreaRef.current.setSelectionRange(selectionStart + 1, selectionStart + 1);
     }
 
     function handleKeyDown(e)
     {
-        if (e.key === 'Enter') createLine(focusedLine);
-        if (e.key === 'ArrowUp') moveLine(e, 'up');
-        if (e.key === 'ArrowDown') moveLine(e, 'down');
-        if (e.key === 'Delete') deleteLine(e, false);
-        if (e.key === 'Backspace' && e.target.selectionStart === 0) deleteLine(e, true);
-    };
+        if (e.key === 'Tab') addTab(e);
+    }
 
+    // HTML
     return (
         <div className="Textfield">
-            {lines.map((line) => (
-                <div className="Textfield__Line" id={line.id}>
-                    <p className="Textfield__LineNumber">{line.id}</p>
-                    <input
-                        className="Textfield__Input"
-                        id={`Textfield__Input${line.id}`}
-                        value={line.content}
-                        onChange={(e) => manageInput(e, line.id)}
-                        onKeyDown={handleKeyDown}
-                        onFocus={() => setFocusedLine(line.id)}
-                    />
-                </div>
-            ))}
+            <div className="Textfield__LineNumbers" ref={lineNumbersRef}>
+                {lineNumbers.map((line, index) => <p class="Textfield__LineNumber" key={index}>{index + 1}</p>)}
+            </div>
+            <textarea className="Textfield__Textarea" ref={textAreaRef} spellCheck="false" onChange={(e) => handleTextareaTextChange(e)} onScroll={handleTextareaScroll} onKeyDown={(e) => {handleKeyDown(e)}}></textarea>
         </div>
     );
 }
